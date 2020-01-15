7abc7958ed8541ca20480575d224e28484c13af4
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 15:06:54 +0300

    Create Proje.kt

---
+++ Proje.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+
+import java.io.PrintStream
+import java.util.Scanner
+import java.util.*
+
+fun main (args : Array<String>)
+{
+   var ogrenci_not = 80
+
+   if( 85 <= ogrenci_not && ogrenci_not <= 100)
+   {
+      print("Bu öğrencinin harf notu AA dır")
+   }
+
+   else
+   {
+      print("Bu öğrencinin harf notu AA değildir")
+   }
+
+}
