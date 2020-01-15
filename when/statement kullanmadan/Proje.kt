7b2b5c3014b855b372c1ec549d9634f94ae2e2f0
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 15:21:22 +0300

    Create Proje.kt

---
+++ when/statement kullanmadan/Proje.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+
+import java.io.PrintStream
+import java.util.Scanner
+import java.util.*
+
+fun main (args : Array<String>)
+{
+  val aylar = 7
+
+   when(aylar){
+
+      1-> print("Ocak")
+      2-> print("Şubat")
+      3-> print("Mart")
+      4-> print("Nisan")
+      5-> print("Mayıs")
+      6-> print("Haziran")
+      7-> print("Temmuz")
+      8-> print("Ağustos")
+      9-> print("Eylül")
+   }
+}
